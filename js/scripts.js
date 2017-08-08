7391de02f58df2b5b186faa2df9c398d44af3d48
Daniel Garcia and Sarah Lindmar <sarah@>
2017-08-08 16:52:23 -0700

    adjust row and well size, add background color to wells

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,97 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $("#clickable1").click(function() {
     //$("hidden1").slideToggle();
-    $(".definition").slideToggle();
+    $("#definition1").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable2").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition2").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable3").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition3").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable4").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition4").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable5").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition5").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable6").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition6").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable7").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition7").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable8").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition8").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable9").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition9").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable10").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition10").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable11").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition11").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable12").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition12").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable13").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition13").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable14").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition14").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable15").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition15").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable16").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition16").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable17").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition17").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable18").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition18").slideToggle();
+    //$("hidden1").slideToggle();
+  });
+  $("#clickable19").click(function() {
+    //$("hidden1").slideToggle();
+    $("#definition19").slideToggle();
     //$("hidden1").slideToggle();
   });
 });
